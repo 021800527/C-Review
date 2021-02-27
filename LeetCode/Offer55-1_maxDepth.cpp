447274207ca7e31d71823be687f231b8997c8e6d
021800527 <978621025@qq.com>
2021-02-27 16:22:56 +0800

    继续冲

--- LeetCode/Offer55-1_maxDepth.cpp
+++ LeetCode/Offer55-1_maxDepth.cpp
@@ -20,6 +20,6 @@ class Solution {
 public:
     int maxDepth(TreeNode* root) {
         if (root == NULL)  return 0;
-        return max(maxDepth(root->left),maxDepth(root->right)+1);
+        return max(maxDepth(root->left),maxDepth(root->right))+1;
     }
 };
