5bde65079a8b191734f3f1e266b377574a3df27e
021800527 <978621025@qq.com>
2021-03-04 14:40:59 +0800

    线索二叉树需要提高

--- wangdao/P149_2.18.cpp
+++ wangdao/P149_2.18.cpp
@@ -9,3 +9,53 @@
  * 若f无左子女，则顺其前驱找双亲的双亲，一直找到双亲有左子女（这时左子女是p的前驱）。
  * 还有一个情况，若p是中序遍历的第一个结点，则结点p在中序和后序下均无前驱
  */
+#include <iostream>
+using namespace std;
+typedef struct BiTNode{
+    int val;
+    struct BiTNode *left,*right;
+    int rtag;
+    int ltag;
+}BiTNode,*BiTree;
+/*
+ * 在中序线索二叉树t中，求指定结点p在后续下的前驱结点q
+ */
+BiTree InPostPre(BiTree t,BiTree p){
+    BiTree q;
+    /*
+     * 若p有右子女，则右子女是其后序前驱
+     */
+    if (p->rtag == 0)
+    {
+        q = p -> right;
+    }
+    /*
+     * 若p只有左子女，则左子女是其后序前驱
+     */
+    else if (p -> ltag ==0)
+        q = p -> left;
+    /*
+     * p是中序序列第一节点，无后序前驱
+     */
+    else if (p -> left == NULL)
+        q = NULL;
+    /*
+     * 顺左线索向上找p的祖先，若存在，再找祖先的左子女
+     */
+    else
+    {
+        while (p->ltag == 1 && p->right == NULL)
+            p = p -> left;
+        /*
+         * p结点的祖先的左子女是其后序前驱
+         */
+        if (p->ltag == 0)
+            q = p -> left;
+        /*
+         * 仅有单支树（p是叶子），已到根结点，p无后序前驱
+         */
+        else
+            q = NULL;
+    }
+    return q;
+}
