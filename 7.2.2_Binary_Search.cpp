856406973a83a5142faceb033ff962b8bdb4a8a5
021800527 <978621025@qq.com>
2021-03-25 20:26:55 +0800

    看书要认真啊

--- 7.2.2_Binary_Search.cpp
+++ 7.2.2_Binary_Search.cpp
@@ -8,7 +8,7 @@ int Binary_Search(SeqList L,int key){
     int low = 0;
     int high = L.TableLen-1;
     int mid;
-    while (low<high)
+    while (low<=high)
     {
         mid = (low+high)/2;
         if(L.elem[mid]==key)
