dd3a8593e8567ee612593ae51342ddd960b007f8
021800527 <978621025@qq.com>
2021-03-30 17:17:37 +0800

    int 别乱用

--- LeetCode/Num912_sortArray.cpp
+++ LeetCode/Num912_sortArray.cpp
@@ -4,6 +4,15 @@ using namespace std;
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-
+        int i, j;
+        int flag;
+        for (i = 1; i <nums.size(); i++) {
+            flag = nums[i];
+            for (j = i - 1;j>=0 && flag < nums[j]; j--) {
+                nums[j + 1] = nums[j];
+            }
+            nums[j+1] = flag;
+        }
+        return nums;
     }
 };
