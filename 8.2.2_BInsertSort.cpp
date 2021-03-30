2b77e6b42b8d6c8078c6ea3b9af99106a63fd0bf
021800527 <978621025@qq.com>
2021-03-30 17:43:08 +0800

    int 别乱用

--- 8.2.2_BInsertSort.cpp
+++ 8.2.2_BInsertSort.cpp
@@ -19,8 +19,8 @@ void BInsertSort(ElemType A[],int n){
                low = mid + 1;
        }
         for ( j = i-1; j >= high+1 ; j--) {
-            A[j+1] = A[j];
-            A[high+1] = A[0];
+            A[j + 1] = A[j];
         }
+        A[high+1] = A[0];
     }
 }
--- LeetCode/Num912_sortArray.cpp
+++ LeetCode/Num912_sortArray.cpp
@@ -3,7 +3,29 @@
 using namespace std;
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
+    vector<int> BinInsertSortArray(vector<int>& nums) {
+        int i,j;
+        int low,high,mid;
+        int flag;
+        for (i = 1; i <nums.size() ; i++) {
+            flag = nums[i];
+            low = 0;
+            high = i -1;
+            while (low<=high){
+                mid = (low+high)/2;
+                if (nums[mid]>flag)
+                    high = mid-1;
+                else
+                    low = mid + 1;
+            }
+            for (j = i-1;  j>=high+1 ; j--) {
+                nums[j+1] = nums[j];
+            }
+            nums[high+1] = flag;
+        }
+    }
+
+    vector<int> InsertSortArray(vector<int>& nums) {
         int i, j;
         int flag;
         for (i = 1; i <nums.size(); i++) {
